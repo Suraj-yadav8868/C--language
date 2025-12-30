a37fa16d0c2ccf5cce24008d4768f2f6e6bee7f6
Surya <mrsurajyadavdj@gmail.com>
2025-12-30 16:22:11 +0530

    Add program to print numbers from 1 to N
    Implement a program to print numbers from 1 to N using a for loop.

---
+++ q-3.c
@@ -0,0 +1,13 @@
+//WAP to print 1 to N using for loop
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    return 0;
+}
